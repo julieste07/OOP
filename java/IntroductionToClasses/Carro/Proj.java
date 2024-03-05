dd894bc10b497c7299b45b1c980982567da95c85
julieste07 <160774443+julieste07@users.noreply.github.com>
2024-03-05 10:21:07 -0500

    Proj.java

--- java/IntroductionToClasses/Carro/Proj.java
+++ java/IntroductionToClasses/Carro/Proj.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package proj;
 
-/**
- *
- * @author Estudiantes
- */
 public class Proj {
 int Año;
   String Marca;
